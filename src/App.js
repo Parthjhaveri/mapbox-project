b849d9663357b9ccea62d00b061033dd4ffd1b05
Parthjhaveri <parthjhaveri209@gmail.com>
2020-03-28 23:40:35 -0400

    edited readme

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  let [color, setCount] = useState(0);
 
   useEffect(() => {
-    document.body.style.backgroundColor = color;
+    
   })
 
   return (
     <div className="App">
-      <button onClick={() => setCount(() => {
-        color = 'red';
-      })}>
-        Click me
-      </button>
+      
     </div>
   );
 }

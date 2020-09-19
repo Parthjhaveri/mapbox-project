08f0f7bb720f3db7586175c41934c0ff6192cc79
Parth Jhaveri <Parthjhaveri@users.noreply.github.com>
2020-09-19 01:47:20 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('./config');
+const MongoClient = require('mongodb').MongoClient;
+
 // IMPORT AND INITIALIZE EXPRESS
 const fetch = require('node-fetch');
 var express = require ('express');
@@ -9,18 +12,64 @@ var port 	= 9000;
 const server = app.listen(port);
 const io = socketIo(server);
 
+// MONGO CONNECTION STRING
+const mongoUrl = process.env.MONGO_URL;
 
-io.on('connection', function(socket){
-  	// GET ALL EARTHQUAKES
-	fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson')
+// CONNECT TO MONGO DB USING MONGO CLIENT
+MongoClient.connect(mongoUrl, { useUnifiedTopology: true }, (err, client) => {
+  
+  	// DATABASE AND COLLECTION
+  	const db = client.db('earthquake_alert');
 
-	.then(resp => resp.json())
+	/* 
+		ALL QUAKES PAST HOUR: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson
+		MAG 4.5+ PAST HOUR: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson
+		MAG 2.5+ PAST HOUR: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson
+		MAG 1.0+ PAST HOUR: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson
+	*/
 
-	.then((json) => {	
-		console.log(json);
-		socket.emit('all_quakes_event', json);
-	})
-	.catch(err => console.log(err));
+	io.on('connection', function(socket){
+
+		setInterval(() => {			
+			// GET ALL EARTHQUAKES
+			fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+
+			.then(resp => resp.json())
+
+			.then((json) => {
+				// console.log(json);
+
+				// PUSH UNIQUE QUAKES TO DATABASE
+				const eq_collection = db.collection('quake_collection');
+
+				json.features.forEach((el, idx) => {
+
+					// SET AN INDEX FOR THE 'ID' FIELD SO THAT IT CAN BE UNIQUE
+					eq_collection.createIndex({id: 1}, {unique: true})		
+					try {
+						eq_collection.insertMany(json.features); // INSERT ONLY UNIQUE QUAKES
+					} catch(e) {
+						console.log(e);
+					}
+				});
+
+			})
+			.catch(err => console.log(err));		
+
+		}, 5000);
+
+		setInterval(() => {
+			// GET UNIQUE QUAKES FROM DATABASE AND SEND TO FRONTEND VIA SOCKET		
+			const eq_collection = db.collection('quake_collection');
+
+			// eq_collection.find().forEach((indv_quake) => {
+			// 	console.log(indv_quake);
+			// });
+
+			const latest_earthquakes = eq_collection.distinct('id');
+			socket.emit('latest_quakes', latest_earthquakes);	
+		}, 6000);
+	}); // END SOCKET IO FUNCTION
 });
 
 // TELL THE SERVER WHAT PORT TO LISTEN TO
